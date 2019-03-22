671dec86057889466dc57dad9486f2f209bc5d03
Martin Campagner <martincampagner@hotmail.com>
2019-03-22 08:37:06 -0300

    Añadido botón de compra. Cero estilos

--- src/index.js
+++ src/index.js
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-function CarInfo() {
-  return(
-    <div>
-      <p>This is a car.</p>
-      <ul>
-        <li>This is its type.</li>
-        <li>This is its color.</li>
-        <li>This is its capacity.</li>
-        <li>This is the engine specifications.</li>
-      </ul>
-    </div>
-  )
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      brand: 'Toyota', 
+      model: 'Hilux', 
+      engine: '2.4 l D-4D', 
+      doors: 'Four',
+      type: '4x4'  
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <h1>{this.state.brand} {this.state.model}</h1>
+        <p>{this.state.engine}</p>
+        <p>{this.state.doors}</p>
+        <p>Type: {this.state.type}</p>
+      </div>
+    );
+  }
 }
-ReactDOM.render(<CarInfo />, document.getElementById('root'));
+
+const Button = props => {
+  return <button onClick={props.onClickHandler}>{props.text}</button>
+}
+
+const BuyButton = props => {
+  return <Button text="Buy car" onClickHandler={props.onClickHandler} />
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { boughtCars: 0 }
+  }
+
+  buyCar = event => {
+    this.setState(state => ({
+      boughtCars: state.boughtCars + 1
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <Car/>
+        <BuyButton
+            onClickHandler = {this.buyCar}
+        />
+        <p>You have bought {this.state.boughtCars} cars.</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
